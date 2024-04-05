99cd222dfab1e2d4b6573495f225ea6d200c5401
CodingVillain <cheolwan.park552@gmail.com>
2024-04-05 12:34:29 +0900

    added progress bar during rendering via BruteforceRenderer
    implemented main.rs

--- raytracer/src/renderer/bruteforce.rs
+++ raytracer/src/renderer/bruteforce.rs
@@ -1,21 +1,34 @@
+use indicatif::ProgressBar;
+
 use super::{ColorSampler, ImageOptions, Renderer};
 use crate::{camera::Camera, hittable::Hittable, image::Image, math::vec3::Vec3, random::random_float, Float};
 
 pub struct BruteForceRenderer {
-    pub color_sampler: Box<dyn ColorSampler>,
-    pub samples_per_pixel: u32,
-    pub max_depth: u32,
+    color_sampler: Box<dyn ColorSampler>,
+    samples_per_pixel: u32,
+    max_depth: u32,
+    verbose: bool,
 }
 
 impl BruteForceRenderer {
     pub fn new(color_sampler: Box<dyn ColorSampler>, samples_per_pixel: u32, max_depth: u32) -> BruteForceRenderer {
-        BruteForceRenderer { color_sampler, samples_per_pixel, max_depth }
+        BruteForceRenderer { color_sampler, samples_per_pixel, max_depth, verbose: true }
+    }
+
+    pub fn set_verbose(&mut self, verbose: bool) {
+        self.verbose = verbose;
     }
 }
 
 impl Renderer for BruteForceRenderer {
     fn render(&self, camera: Camera, world: Box<dyn Hittable>, image_options: ImageOptions) -> Image {
         let mut image = Image::new_with_gamma_correction(image_options.width, image_options.height, 2.2);
+
+        let progressbar = if self.verbose { 
+            Some(ProgressBar::new(image_options.width as u64 * image_options.height as u64))
+        } else {
+            None
+        };
 
         for j in 0..image_options.height {
             for i in 0..image_options.width {
@@ -31,7 +44,14 @@ impl Renderer for BruteForceRenderer {
                 }
 
                 image.set_pixel(i, j, pixel_color.into());
+                if let Some(bar) = &progressbar {
+                    bar.inc(1);
+                }
             }
+        }
+
+        if let Some(bar) = &progressbar {
+            bar.finish();
         }
 
         image
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use raytracer::{camera::Camera, hittable::{list::HittableList, sphere::Sphere}, math::vec3::Vec3, renderer::{Renderer, bruteforce::BruteForceRenderer, colorsampler::GeneralSampler, ImageOptions}, Float};
+
 fn main() {
-    println!("Hello, world!");
+    let width = 400_usize;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_options = ImageOptions::new(width, (width as Float / aspect_ratio) as usize);
+    let camera = Camera::new(
+        1.0, 
+        Vec3::zero(), 
+        Vec3::new(0.0, 0.0, -1.0), 
+        Vec3::new(0.0, 1.0, 0.0),
+         90.0, 
+         aspect_ratio
+    );
+
+    let renderer = BruteForceRenderer::new(
+        Box::new(GeneralSampler::new()), 
+        50, 
+        10
+    );
+
+    let mut world = Box::new(HittableList::new());
+    world.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+    world.push(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
+
+    let renderer = BruteForceRenderer::new(Box::new(GeneralSampler::new()), 50, 10);
+    let image = renderer.render(camera, world, image_options);
+    image.save("output.png")
 }
